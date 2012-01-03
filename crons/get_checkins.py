694c40a7525cef27fc4ce176abe233400a989f2f
Brian Seitz <brnstz@gmail.com>
2012-01-02 20:14:03 -0500

    - convert to spaces

--- crons/get_checkins.py
+++ crons/get_checkins.py
@@ -6,6 +6,7 @@ import time
 import json
 
 CONF = json.load(open('/usr/local/hfa/conf/hfa.conf'))
+#CONF = json.load(open('/home/bseitz/git/hfa/conf/hfa.conf'))
 ROOT  = CONF['root']
 TOKEN = CONF['token']
 API   = 'https://api.foursquare.com/v2/venues/%s/herenow?oauth_token=%s&limit=%d&offset=%d&afterTimestamp=%d&v=%s'
@@ -24,48 +25,48 @@ VENUES = CONF['stadiums']
 NFL_VENUES = CONF['stadiums_nfl']
 
 def get_url(venue, afterTimestamp=0, limit=DEFAULT_LIMIT, offset=0):
-	return API % (venue, TOKEN, limit, offset, afterTimestamp, VERSION)
+    return API % (venue, TOKEN, limit, offset, afterTimestamp, VERSION)
 
 def get_last_ts():
-	fh = open(TS_FILE, 'r')
-	ts = int(fh.read())
-	fh.close()
-	
-	return ts
+    fh = open(TS_FILE, 'r')
+    ts = int(fh.read())
+    fh.close()
+    
+    return ts
 
 def save_ts(ts):
-	fh = open(TS_FILE + '.tmp', 'w')
-	fh.write(str(ts))
-	fh.close()
-	os.rename(TS_FILE + '.tmp', TS_FILE)
+    fh = open(TS_FILE + '.tmp', 'w')
+    fh.write(str(ts))
+    fh.close()
+    os.rename(TS_FILE + '.tmp', TS_FILE)
 
 def run_venues(venues_dict, league):
-	last_ts = get_last_ts()
-	ts = int(time.time())
+    last_ts = get_last_ts()
+    ts = int(time.time())
 
-	for hfc in venues_dict.keys():
-		url = get_url(venues_dict[hfc], last_ts - TS_OFFSET)
+    for hfc in venues_dict.keys():
+        url = get_url(venues_dict[hfc], last_ts - TS_OFFSET)
 
-		# Get data from 4sq
-		url_fh  = urllib.urlopen(url)
-		data = url_fh.read()
-		url_fh.close()
+        # Get data from 4sq
+        url_fh  = urllib.urlopen(url)
+        data = url_fh.read()
+        url_fh.close()
 
-		if league == 'nfl':
-			mydir  = '%s/data/nfl/checkins/%d/%s' % (ROOT, ts, hfc)
-			myfile = '%s/data.json' % (mydir)
-		else:	
-			mydir  = '%s/data/checkins/%d/%s' % (ROOT, ts, hfc)
-			myfile = '%s/data.json' % (mydir)
-		
-		ensure_dir(myfile)
+        if league == 'nfl':
+            mydir  = '%s/data/nfl/checkins/%d/%s' % (ROOT, ts, hfc)
+            myfile = '%s/data.json' % (mydir)
+        else:    
+            mydir  = '%s/data/checkins/%d/%s' % (ROOT, ts, hfc)
+            myfile = '%s/data.json' % (mydir)
+        
+        ensure_dir(myfile)
 
-		fh = open(myfile + '.tmp', 'w')
-		fh.write(data)
-		fh.close()
-		os.rename(myfile + '.tmp', myfile)
+        fh = open(myfile + '.tmp', 'w')
+        fh.write(data)
+        fh.close()
+        os.rename(myfile + '.tmp', myfile)
 
-	save_ts(ts)	
+    save_ts(ts)    
 
 def ensure_dir(f):
     d = os.path.dirname(f)
